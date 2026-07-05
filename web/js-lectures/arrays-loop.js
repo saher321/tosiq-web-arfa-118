d9d0519f0c34cebfddd1aad496ea69adcc27531d
tosiq <pnymeet@gmail.com>
2026-07-05 11:22:21 +0500

    arrays

---
+++ web/js-lectures/arrays-loop.js
@@ -0,0 +1,17 @@
+// indexes          0         1        2
+const colors = [ "Purple", "Black", "White" ]
+
+console.log(colors[2])
+
+for (let i = 0; i < colors.length; i++ ) {
+    if (colors[i] == "Banned") {
+        break;
+    }
+    console.log(colors[i])
+}
+// loop
+// for
+// while
+// foreach
+// do-while x
+// map => arrays

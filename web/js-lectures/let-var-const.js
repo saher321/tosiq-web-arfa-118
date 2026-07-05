c213ce173860467ac7f24a2ef1cc141522384785
tosiq <pnymeet@gmail.com>
2026-07-05 10:37:45 +0500

    let var const

---
+++ web/js-lectures/let-var-const.js
@@ -0,0 +1,14 @@
+var name = "Ali"
+var name = "New name"
+console.log(name)
+// var can be redeclare and re-initialize
+
+let age = 32
+age = 34
+console.log(age)
+// let can not be redeclare but re-initialize
+
+const status = "online"
+// status = "offline"
+console.log(status)
+// const can not be redeclare or re-initialize

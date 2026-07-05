d2ba7fc883cc5efe1a78d7a3a04cc598e475f4f8
tosiq <pnymeet@gmail.com>
2026-07-05 10:55:46 +0500

    global vs block scope

--- web/js-lectures/let-var-const.js
+++ web/js-lectures/let-var-const.js
@@ -1,14 +1,25 @@
+// console.log(name)
 var name = "Ali"
 var name = "New name"
-console.log(name)
 // var can be redeclare and re-initialize
 
+// console.log(age) // Hoeisted
 let age = 32
 age = 34
-console.log(age)
+// console.log(age)
 // let can not be redeclare but re-initialize
 
 const status = "online"
+// console.log(status)
 // status = "offline"
-console.log(status)
 // const can not be redeclare or re-initialize
+
+// let y = 44
+if (true){
+    var x = 10
+    let y = 20
+    const z = 30
+}
+console.log(x) // global scope
+console.log(y) // block scope
+console.log(z) // block scrope

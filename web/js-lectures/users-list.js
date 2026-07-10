0f228b8f8edcc8dbfca70848b739f1f52dabb32a
Tosiq <pnymeet@gmail.com>
2026-07-10 11:19:22 +0500

    user list

---
+++ web/js-lectures/users-list.js
@@ -0,0 +1,38 @@
+// function addNewUser() {
+//     let n = document.getElementById("name").value
+//     let userList = document.getElementById("list")
+//     // innerText
+//     // innerHTML
+//     userList.innerHTML += `<li> ${n} </li>`
+// }
+
+const allUsers = [
+        {id: 1, name: "Ali", email: "ali@gmail.com", status: true, role: "User" },
+        {id: 2, name: "Alice", email: "alice@gmail.com", status: true, role: "Admin" },
+        {id: 3, name: "Clark", email: "clark@gmail.com", status: false, role: "User" },
+        {id: 4, name: "John", email: "john@gmail.com", status: false, role: "Vendor" },
+        {id: 5, name: "Samantha", email: "sam@gmail.com", status: true, role: "User" },
+        {id: 6, name: "Asif", email: "asif@gmail.com", status: true, role: "Vendor" },
+    ]
+
+function insertUser(user) {
+    getAllUser(allUsers)
+}
+getAllUser(allUsers)
+function getAllUser(list) {
+    
+    let userList = document.getElementById("userList")
+
+    for(let i = 0; i <= list.length; i++) {
+        // console.log(allUsers[0])
+        userList.innerHTML += `
+        <tr>
+            <td> ${list[i].id} </td>
+            <td> ${list[i].name} </td>
+            <td> ${list[i].email} </td>
+            <td> ${list[i].status ? "Active":"Inactive"} </td>
+            <td> ${list[i].role} </td>
+        </tr>
+        `
+    }
+}

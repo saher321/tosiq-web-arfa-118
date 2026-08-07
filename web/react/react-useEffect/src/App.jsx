15fe150cca089bec2f8096b41fa4295675288f48
Tosiq <pnymeet@gmail.com>
2026-08-07 11:45:45 +0500

    axios pkg added, link added, url calling

--- web/react/react-useEffect/src/App.jsx
+++ web/react/react-useEffect/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { USER_API } from './utils/urls.js'
 
 const App = () => {
   const [counter, setCounter] = useState(1)
-  
+  const [users, setUsers] = useState([])
+
   const count = () => {
     setCounter(counter + 1)
   }
@@ -21,14 +24,52 @@ const App = () => {
     }
   }, [counter])
 
+  const getUsers = async () => {
+    const response = await axios.get(USER_API)
+    // const response = await fetch(USER_API, 
+    //   {
+    //     method: "Post",
+    //     body: "form value"
+    //   }
+    // )
+    // const result = await response.json()
+    // console.log(result)
+    console.log(response.data, response.status)
+    if (response.status == 200) {
+      setUsers(response.data.users)
+    }
+    
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
   return (
   <div>
     <h1>useEffect()</h1>
     <button onClick={count}>
       Click me [{counter}]
     </button>
+    <hr />
+
+    <div>
+      <h1>User's List</h1>
+      <ol>
+        {users.map((user, i) => {
+          return (
+            <li key={i}>{user.firstName} {user.lastName}</li>
+          )
+        })}
+      </ol>
+    </div>
   </div>
   )
 }
 
-export default App;
+export default App
+
+// Get          => fetch
+// Post         => send data to server
+// Delete       => delete data from database
+// Patch / Put  => data update

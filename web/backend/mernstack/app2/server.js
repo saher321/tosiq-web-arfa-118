a05940a3cfee257496bfa41bf7a78d0dd399e511
Tosiq <pnymeet@gmail.com>
2026-08-21 11:42:37 +0500

    users api

--- web/backend/mernstack/app2/server.js
+++ web/backend/mernstack/app2/server.js
@@ -4,6 +4,20 @@ import express from 'express'
 // const express = require("express")
 
 const app = express()
+const PREFIX = '/api/v1'
+const users = [
+    {id: 101, name: "Kloe", email:"kloe@email.com"},
+    {id: 102, name: "July", email:"july@email.com"},
+    {id: 103, name: "Alex", email:"alx@email.com"}
+]
+
+// end-point: http://localhost:5000/api/v1/users
+app.get(`${PREFIX}/users`, (req, res) => {
+    return res.send({
+        status: true,
+        users
+    })
+})
 
 // http://localhost:5000/health
 
